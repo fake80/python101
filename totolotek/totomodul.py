a63ba7ca30f62c856b947eec86bcb10a7f9a3525
john doe <fake80@op.pl>
2018-10-02 20:31:07 +0200

    update print

--- totolotek/totomodul.py
+++ totolotek/totomodul.py
@@ -9,16 +9,16 @@ def ustawienia():
     """Funkcja pobiera nick użytkownika, ilość losowanych liczb, maksymalną
     losowaną wartość oraz ilość typowań. Ustawienia zapisuje."""
 
-    nick = raw_input("Podaj nick: ")
+    nick = input("Podaj nick: ")
     nazwapliku = nick + ".ini"
     gracz = czytaj_ust(nazwapliku)
     odp = None
     if gracz:
-        print "Twoje ustawienia:"
-        print "Liczb:", gracz[1]
-        print "Z Maks:", gracz[2]
-        print "Losowań:", gracz[3]
-        odp = raw_input("Zmieniasz (t/n)? ")
+        print ("Twoje ustawienia:")
+        print ("Liczb:", gracz[1])
+        print ("Z Maks:", gracz[2])
+        print ("Losowań:", gracz[3])
+        odp = input("Zmieniasz (t/n)? ")
 
     if not gracz or odp.lower() == "t":
         while 1:
@@ -26,12 +26,12 @@ def ustawienia():
                 ile = int(raw_input("Podaj ilość typowanych liczb: "))
                 maks = int(raw_input("Podaj maksymalną losowaną liczbę: "))
                 if ile > maks:
-                    print "Błędne dane!"
+                    print ("Błędne dane!")
                     continue
                 ilelos = int(raw_input("Ile losowań: "))
                 break
             except:
-                print "Błędne dane!"
+                print ("Błędne dane!")
                 continue
         gracz = zapisz_ust(nazwapliku, [nick, str(ile), str(maks), str(ilelos)])
 
@@ -68,14 +68,14 @@ def losujliczby(ile, maks):
 
 def pobierztypy(ile, maks):
     """Funkcja pobiera od użytkownika jego typy wylosowanych liczb"""
-    print "Wytypuj", ile, "z", maks, " liczb: "
+    print ("Wytypuj", ile, "z", maks, " liczb: ")
     typy = set()
     i = 0
     while i < ile:
         try:
             typ = int(raw_input("Podaj liczbę " + str(i + 1) + ": "))
         except ValueError:
-            print "Błędne dane!"
+            print ("Błędne dane!")
             continue
 
         if 0 < typ <= maks and typ not in typy:
@@ -87,11 +87,11 @@ def pobierztypy(ile, maks):
 def wyniki(liczby, typy):
     trafione = liczby & typy
     if trafione:
-        print "\nIlość trafień: ", len(trafione)
-        print "Trafione liczby: ", trafione
+        print ("\nIlość trafień: ", len(trafione))
+        print ("Trafione liczby: ", trafione)
     else:
-        print "Brak trafień. Spróbuj jeszcze raz!"
-    print "\n" + "x" * 40 + "\n"  # wydrukuj 40 znaków x
+        print ("Brak trafień. Spróbuj jeszcze raz!")
+    print ("\n" + "x" * 40 + "\n" ) # wydrukuj 40 znaków x
 
     return len(trafione)
 
